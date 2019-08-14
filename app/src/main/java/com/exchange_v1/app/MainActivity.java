5160621fdb1ed649dd4bcd2d0786f09eb26f7118
simidas <443516438@qq.com>
2019-08-14 23:29:41 +0800

    提交注释

--- app/src/main/java/com/exchange_v1/app/MainActivity.java
+++ app/src/main/java/com/exchange_v1/app/MainActivity.java
@@ -7,6 +7,7 @@ import com.exchange_v1.R;
 
 public class MainActivity extends Activity {
 
+    //做个注释
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
