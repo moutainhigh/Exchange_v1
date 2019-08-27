3e93004c0bda1c907813c4078bbe9ac6eefff026
simidas <443516438@qq.com>
2019-08-27 23:58:19 +0800

    邮箱验证界面

--- app/src/main/java/com/exchange_v1/app/activity/EmailVerifActivity.java
+++ app/src/main/java/com/exchange_v1/app/activity/EmailVerifActivity.java
@@ -1,16 +1,18 @@
 package com.exchange_v1.app.activity;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.exchange_v1.R;
 import com.exchange_v1.app.base.BaseActivity;
 
 //邮箱验证接口
 public class EmailVerifActivity extends BaseActivity implements View.OnClickListener {
-    @Override
-    public void onClick(View v) {
-
-    }
+    private EditText etEmail;
+    private EditText etMsgCode;
+    private TextView tvSendMsg;
+    private TextView tvSubmit;
 
     @Override
     protected int getContentViewId() {
@@ -19,12 +21,17 @@ public class EmailVerifActivity extends BaseActivity implements View.OnClickList
 
     @Override
     protected void findViews() {
+        etEmail = (EditText) findViewById(R.id.et_email);
+        etMsgCode = (EditText) findViewById(R.id.et_msg_code);
+        tvSendMsg = (TextView) findViewById(R.id.tv_send_msg);
+        tvSubmit = (TextView) findViewById(R.id.tv_submit);
 
     }
 
     @Override
     protected void initGetData() {
-
+        titleView.setBackBtn();
+        titleView.setTitle("邮箱验证");
     }
 
     @Override
@@ -34,6 +41,11 @@ public class EmailVerifActivity extends BaseActivity implements View.OnClickList
 
     @Override
     protected void init() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
