9f3fa9dac376bb55b2242113eb002cbf9b800100
simidas <443516438@qq.com>
2019-08-28 00:06:11 +0800

    申请服务商

--- app/src/main/java/com/exchange_v1/app/activity/ApplyServiceActivity.java
+++ app/src/main/java/com/exchange_v1/app/activity/ApplyServiceActivity.java
@@ -1,16 +1,17 @@
 package com.exchange_v1.app.activity;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.exchange_v1.R;
 import com.exchange_v1.app.base.BaseActivity;
 
 //申请服务商
 public class ApplyServiceActivity extends BaseActivity implements View.OnClickListener {
-    @Override
-    public void onClick(View v) {
-
-    }
+    private EditText etQq;
+    private EditText etPhone;
+    private TextView tvSubmit;
 
     @Override
     protected int getContentViewId() {
@@ -19,12 +20,16 @@ public class ApplyServiceActivity extends BaseActivity implements View.OnClickLi
 
     @Override
     protected void findViews() {
+        etQq = (EditText) findViewById(R.id.et_qq);
+        etPhone = (EditText) findViewById(R.id.et_phone);
+        tvSubmit = (TextView) findViewById(R.id.tv_submit);
 
     }
 
     @Override
     protected void initGetData() {
-
+        titleView.setBackBtn();
+        titleView.setTitle("申请服务商");
     }
 
     @Override
@@ -34,6 +39,11 @@ public class ApplyServiceActivity extends BaseActivity implements View.OnClickLi
 
     @Override
     protected void init() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
