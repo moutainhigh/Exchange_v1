4a70c2d4e144233593d091da89c6ad4600478cfa
simidas <443516438@qq.com>
2019-08-31 18:14:33 +0800

    首页UI完成

---
+++ app/src/main/java/com/exchange_v1/app/fragment/HomeOrderReceiveFragment.java
@@ -0,0 +1,39 @@
+package com.exchange_v1.app.fragment;
+
+import android.view.View;
+
+import com.exchange_v1.R;
+import com.exchange_v1.app.base.BaseFragment;
+
+public class HomeOrderReceiveFragment extends BaseFragment implements View.OnClickListener {
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.fragment_home_order_receive;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void initGetData() {
+
+    }
+
+    @Override
+    protected void widgetListener() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/exchange_v1/app/fragment/MainHomeFragment.java
+++ app/src/main/java/com/exchange_v1/app/fragment/MainHomeFragment.java
@@ -2,15 +2,29 @@ package com.exchange_v1.app.fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.RadioButton;
 
 import com.exchange_v1.R;
 import com.exchange_v1.app.base.BaseFragment;
+import com.flyco.tablayout.SlidingTabLayout;
+
+import java.util.ArrayList;
 
 /**
  * 首页
  */
 public class MainHomeFragment extends BaseFragment implements View.OnClickListener {
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private HomeVpAdapter mAdapter;
+    private int currentItem = 0;
+
+    private RadioButton radioButton;
+    private boolean radioCheck;
 
     @Override
     protected View getViews() {
@@ -19,7 +33,32 @@ public class MainHomeFragment extends BaseFragment implements View.OnClickListen
 
     @Override
     protected void findViews() {
+        mFragments.add(new HomeOrderReceiveFragment());
+        mFragments.add(new HomeOrderReceiveFragment());
 
+        ViewPager vp = getView(R.id.vp);
+        mAdapter = new HomeVpAdapter(getChildFragmentManager(),mFragments);
+        vp.setOffscreenPageLimit(1);
+        vp.setAdapter(mAdapter);
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        SlidingTabLayout tabLayout = getView(R.id.tl);
+        tabLayout.setViewPager(vp);
+        vp.setCurrentItem(currentItem);
+
+        radioButton = findViewById(R.id.rb_1);
     }
 
 
@@ -28,9 +67,23 @@ public class MainHomeFragment extends BaseFragment implements View.OnClickListen
 
     }
 
+
     @Override
     protected void widgetListener() {
+        radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if (radioCheck) {
+                    radioButton.setChecked(false);
+                    radioCheck = false;
+                }else{
+                    radioButton.setChecked(true);
+                    radioCheck = true;
+                }
+
+            }
+        });
     }
 
     @Override
@@ -50,4 +103,30 @@ public class MainHomeFragment extends BaseFragment implements View.OnClickListen
     }
 
 
+    public class HomeVpAdapter extends FragmentStatePagerAdapter {
+
+        private ArrayList<Fragment> mFragments;
+        private final String[] mTitles = {"接单", "进行中"};
+
+        public HomeVpAdapter(FragmentManager childFragmentManager, ArrayList<Fragment> list) {
+            super(childFragmentManager);
+            this.mFragments = list;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
+
+    }
 }
