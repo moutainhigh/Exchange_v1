b3d9afdb3c6efdc457df7d7411f5b14025ffcbe8
simidas <443516438@qq.com>
2019-08-31 23:36:33 +0800

    退出登录按钮

--- app/src/main/java/com/exchange_v1/app/fragment/MainMineFragment.java
+++ app/src/main/java/com/exchange_v1/app/fragment/MainMineFragment.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 import com.exchange_v1.R;
+import com.exchange_v1.app.activity.LoginActivity;
 import com.exchange_v1.app.base.BaseFragment;
+import com.exchange_v1.app.utils.IntentUtil;
 
 
 /**
@@ -15,6 +18,7 @@ import com.exchange_v1.app.base.BaseFragment;
  */
 public class MainMineFragment extends BaseFragment implements OnClickListener {
 
+    private TextView tvExitLogin;
 
     @Override
     protected View getViews() {
@@ -23,7 +27,7 @@ public class MainMineFragment extends BaseFragment implements OnClickListener {
 
     @Override
     protected void findViews() {
-
+        tvExitLogin = findViewById(R.id.tv_exit_login);
     }
 
 
@@ -34,6 +38,7 @@ public class MainMineFragment extends BaseFragment implements OnClickListener {
 
     @Override
     protected void widgetListener() {
+        tvExitLogin.setOnClickListener(this);
     }
 
     @Override
@@ -48,7 +53,13 @@ public class MainMineFragment extends BaseFragment implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.tv_exit_login:
+                IntentUtil.gotoActivity(context, LoginActivity.class);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
