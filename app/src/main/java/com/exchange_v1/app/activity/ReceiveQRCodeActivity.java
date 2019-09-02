70be3388acbd9d93f3ba4db41c10d746ac54846f
Simidas <443516438@qq.com>
2019-09-02 19:26:40 +0800

    文件上传接口

--- app/src/main/java/com/exchange_v1/app/activity/ReceiveQRCodeActivity.java
+++ app/src/main/java/com/exchange_v1/app/activity/ReceiveQRCodeActivity.java
@@ -1,7 +1,10 @@
 package com.exchange_v1.app.activity;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -10,7 +13,10 @@ import com.exchange_v1.app.base.BaseActivity;
 import com.exchange_v1.app.bean.ResponseBean;
 import com.exchange_v1.app.biz.FileBiz;
 import com.exchange_v1.app.network.RequestHandle;
-import com.exchange_v1.app.utils.IntentUtil;
+import com.exchange_v1.app.utils.Logger;
+import com.exchange_v1.app.utils.StringUtil;
+import com.exchange_v1.app.utils.ToastUtil;
+import com.wildma.pictureselector.PictureSelector;
 
 //通用收款码界面
 public class ReceiveQRCodeActivity extends BaseActivity implements View.OnClickListener {
@@ -20,6 +26,8 @@ public class ReceiveQRCodeActivity extends BaseActivity implements View.OnClickL
     private EditText etIdCard;
     private EditText etUID;
     private TextView btSubmit;
+    private ImageView ivPic;
+    private String picturePath;
 
     @Override
     protected int getContentViewId() {
@@ -33,6 +41,7 @@ public class ReceiveQRCodeActivity extends BaseActivity implements View.OnClickL
         etIdCard = (EditText) findViewById(R.id.et_id_card);
         etUID = (EditText) findViewById(R.id.et_UID);
         btSubmit = (TextView) findViewById(R.id.bt_submit);
+        ivPic = findViewById(R.id.iv_pic);
 
     }
 
@@ -69,21 +78,53 @@ public class ReceiveQRCodeActivity extends BaseActivity implements View.OnClickL
     }
 
     private void selectPng() {
-        IntentUtil.gotoActivity(context,SettingUserAvatarActivity.class);
+        PictureSelector
+                .create(thisA, PictureSelector.SELECT_REQUEST_CODE)
+                .selectPicture(true, 200, 200, 1, 1);
+
+//        IntentUtil.gotoActivity(context,SettingUserAvatarActivity.class);
     }
 
     //上传文件接口
-    private void upLoadQrCode() {
-        FileBiz.uploadFile(context, "", new RequestHandle() {
+    private void upLoadQrCode(String path) {
+        FileBiz.uploadFile(context, path, new RequestHandle() {
             @Override
             public void onSuccess(ResponseBean result) {
-
+                Logger.i("上传图片成功");
+                ToastUtil.showToast(context,"收款码已上传");
             }
 
             @Override
             public void onFail(ResponseBean result) {
-
+                ToastUtil.showToast(context,result.getInfo());
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        /*结果回调*/
+        if (requestCode == PictureSelector.SELECT_REQUEST_CODE) {
+            if (data != null) {
+                picturePath = data.getStringExtra(PictureSelector.PICTURE_PATH);
+                ivPic.setVisibility(View.VISIBLE);
+                ivPic.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+                rlQrPng.setVisibility(View.GONE);
+
+                if (!StringUtil.isEmpty(picturePath)){
+                    upLoadQrCode(picturePath);
+                }
+
+                /*使用 Glide 加载图片，由于裁剪后的图片地址是相同的，所以不能从缓存中加载*/
+                /*RequestOptions requestOptions = RequestOptions
+                        .circleCropTransform()
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true);
+                Glide.with(this).load(picturePath).apply(requestOptions).into(mIvImage);*/
+            }
+        }
+    }
+
 }
